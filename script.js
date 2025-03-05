d31c2292383415e6d754792ea50cce3e6253bae2
LELISA WAKTOLA <lalowakt@gmail.com>
2025-03-05 04:56:29 -0800

    more on UX

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let shop=document.getElementById(`shop`);
 let  shopItem=[
     {
         id:"ghjkxdjkh",
@@ -19,15 +20,23 @@ let  shopItem=[
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",  
         price:48,
         img:"images/image3.png"
+    },
+    {
+        id:"kfgdsytrak",
+        name: "red shirrt",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",  
+        price:48,
+        img:"images/image3.png"
     }
 ]
 
-let bascket=[];
+let bascket=JSON.parse(localStorage.getItem("data"))||[];
 let generateShop=()=>{
   return(
      shop.innerHTML=shopItem.map((x)=>{
+        let search=bascket.find((y)=>y.id===x.id)||[];
         return` <div id="product-id-${x.id}" class="item">
-            <img src="${x.img}" width="220" height="220" alt="">
+            <img src="${x.img}" width="219" height="220" alt="">
             <div class="details">
                 <h3>${x.name}</h3>
                 <p>${x.desc}</p>
@@ -35,7 +44,7 @@ let generateShop=()=>{
                     <h2>$ ${x.price}</h2>
                     <div class="buttons">
                         <i class="bi bi-plus" onclick="increment(${x.id})"></i>
-                         <div id=${x.id} class="quantity">0</div>
+                         <div id=${x.id} class="quantity">${search.item===undefined? 0: search.item}</div>
                         <i class="bi bi-dash" onclick="decrement(${x.id})"></i>
                     </div>
                 </div>
@@ -59,6 +68,9 @@ let increment=(id)=>{
     }else{
         search.item+=1;
     }
+
+  
+    localStorage.setItem("data",JSON.stringify(bascket));
    update(selectedItem.id);
 }
 
@@ -70,6 +82,8 @@ let decrement=(id)=>{
     }else{
         search.item-=1;
     }
+    localStorage.setItem("data",JSON.stringify(bascket));
+
    update(selectedItem.id);
 }
 
@@ -81,7 +95,6 @@ let update=(id)=>{
 
 let calculation=()=>{
      let cartAmount=document.getElementById(`cartAmount`);
-
      cartAmount.innerHTML=bascket.map((x)=>x.item).reduce((x,y)=>x+y,0);
 }
 
